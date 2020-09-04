df097ff6c9c340d74fc7cfdcdc75582725a0b7ae
ThePsyk3y <50011738+ThePsyk3y@users.noreply.github.com>
2020-09-04 23:45:16 +0530

    Completed Basic Functionality.

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-const colorController = (function color() {})();
+const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffffff', '#ff00ff', '#ffff00', '#4a505d', '#690420'];
+const btn = document.getElementById('color-changer');
+const colortxt = document.getElementById('color');
+let prevRand;
 
-const UIController = (function UI() {})();
+function getRandomNumber() {
+  let currRand;
+  // * To make sure no color is displayed more than once at a time
+  for (;;) {
+    currRand = Math.floor(Math.random() * colors.length);
+    if (prevRand === currRand);
+    else {
+      prevRand = currRand;
+      break;
+    }
+  }
 
-const appController = (function appCont(colorCtrl, UICtrl) {})(colorController, UIController);
+  return currRand;
+}
+
+btn.addEventListener('click', () => {
+  const randNum = getRandomNumber();
+  document.body.style.backgroundColor = colors[randNum];
+  colortxt.textContent = colors[randNum];
+});
